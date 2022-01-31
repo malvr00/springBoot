a9e326763ad95a1afdf733613e3b980dd3d94ae8
malvr00 <yedn1128@naver.com>
2022-02-01 00:10:44 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
@@ -15,12 +16,19 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager en;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager en) {
-        this.en = en;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+//    private EntityManager en;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager en) {
+//        this.en = en;
+//    }
 
     //    private DataSource dataSource;
 //
@@ -32,16 +40,16 @@ public class SpringConfig {
     // 직접 Bean에 등록하는 방법
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
         // Repository 어노테이션을 활용하지 않고 Bean에 등록해서 사용하면
         // 바꿔치기 용의함.
-        return new JpaMemberRepository(en);
+//        return new JpaMemberRepository(en);               // JPA
 //        return new JdbcMemberRepository(dataSource);        // JDBC Template
 //        return new JdbcMemberRepository(dataSource);      // 순수 JDBC
 //        return new MemoryMemberRepository();              // Memory
-    }
+//    }
 }
